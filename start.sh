1ecab1704c207b0c672151a3d3e18a9e9aa013ac
email <kibetdavidro@gmail.com>
2022-02-07 14:02:19 +0300

    add the the executable file

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='FlSkPItchA@*ppL&iCA^$tio***n'
+export MAIL_USERNAME='kibetdavidro@gmail.com'
+export MAIL_PASSWORD='Kibetrono1996@'
+export DATABASE_URL='postgresql+psycopg2://kibet:KibetFlask@localhost/flask_pitch'
+python3 manage.py server

e6368887c74b17ef675de8ea5da03b29bdfbbdbc
email <kibetdavidro@gmail.com>
2022-02-07 17:41:26 +0300

    deploy to github

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class ProdConfig(Config):
     Args:
     Config: The parent configuration class with General configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://kibet:KibetFlask@localhost/flask_pitch'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://kibet:KibetFlask@localhost/flask_pitch'
 
 
 

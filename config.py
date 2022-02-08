79d1ccc9373b2452cf264ee00a95c7a134daa368
email <kibetdavidro@gmail.com>
2022-02-08 10:19:30 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -21,8 +21,10 @@ class ProdConfig(Config):
     """
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kibet:KibetFlask@localhost/flask'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
 
 
 class TestConfig(Config):
